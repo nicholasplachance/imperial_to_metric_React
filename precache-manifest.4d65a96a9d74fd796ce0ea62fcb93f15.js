435bc82ce4b8ce5a1036a65100bf242754d18ea1
nick lachance <nicholasplachance@gmail.com>
2019-12-17 00:29:50 -0600

    Updates

---
+++ precache-manifest.4d65a96a9d74fd796ce0ea62fcb93f15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99655c28ef5445da064332c959b92f1d",
+    "url": "/nicholasplachance/imperial_to_metric_React/index.html"
+  },
+  {
+    "revision": "ddd5e9201d81caa17754",
+    "url": "/nicholasplachance/imperial_to_metric_React/static/css/main.289467dc.chunk.css"
+  },
+  {
+    "revision": "8302bff6600fb610b222",
+    "url": "/nicholasplachance/imperial_to_metric_React/static/js/2.7e12914a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/nicholasplachance/imperial_to_metric_React/static/js/2.7e12914a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ddd5e9201d81caa17754",
+    "url": "/nicholasplachance/imperial_to_metric_React/static/js/main.ce8cee11.chunk.js"
+  },
+  {
+    "revision": "78099bc4b8bcf96c09b1",
+    "url": "/nicholasplachance/imperial_to_metric_React/static/js/runtime-main.38c0605b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nicholasplachance/imperial_to_metric_React/static/media/logo.5d5d9eef.svg"
+  }
+]);
